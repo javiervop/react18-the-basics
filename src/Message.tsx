7f82594e00bb3747e642c2ae786efa3fc88bc387
Javier Valencia <javier.valencia@app4u.com>
2023-04-22 09:13:25 -0400

    initial sample code

---
+++ src/Message.tsx
@@ -0,0 +1,13 @@
+
+// PascalCasing
+function Message() {
+ // JSX: JavaScript XML
+ // https://babeljs.io/repl
+ const name = 'Javier';
+ if (name)
+    return <h1>Hello { name }</h1>;
+ return <h1>Hello World</h1>
+}
+
+export default Message;
+
