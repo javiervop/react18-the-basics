2a3deb3b6e60a6ea238fcc1b9234f2c7506fb9c1
Javier Valencia <javier.valencia@app4u.com>
2023-05-06 10:59:40 -0400

    http request 55%

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,116 @@
-import { useState } from "react";
-// import Message from "./Message";
-import ListGroup from "./components/ListGroup";
-import Alert from "./components/Alert";
-import Button from "./components/Button/Button";
-import { BsFillCalendarFill } from "react-icons/bs";
-import Like from "./components/Like";
-import Form from "./components/Form";
+import { useEffect, useState } from "react";
+import axios, { AxiosError } from "axios";
 import "./App.css";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import ProductList from "./expense-tracker/components/ProductList";
+
+const connect = () => console.log("Connecting");
+const disconnect = () => console.log("Disconnecting");
+
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0);
-  const items = ["Tulua", "Fenicia", "Cartago", "Cali"];
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
 
-  const [alertVisible, setAlertVisibility] = useState(false);
+  useEffect(() => {
+    // this method return a promise
+    // An object that holds the eventual result or failure
+    // of an asynchronous operation.
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+    const fetchUsers = async () => {
+      try {
+        const res = await axios.get<User[]>(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        setUsers(res.data);
+      } catch (err) {
+        console.log(err);
+        setError((err as AxiosError).message);
+      }
+    };
+
+    fetchUsers();
+
+    /** 
+    axios
+      // get => promise => resp / err
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res.data[0].name);
+        setUsers(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(err.message);
+      });
+    */
+  }, []);
+
+  useEffect(() => {
+    connect();
+    return () => disconnect();
+  });
+
+  const [category, setCategory] = useState("");
+
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "test1", amount: 10, category: "Utilities" },
+    { id: 2, description: "test2", amount: 20, category: "Utilities" },
+    { id: 3, description: "test3", amount: 20, category: "Utilities" },
+    { id: 4, description: "test4", amount: 20, category: "Utilities" },
+  ]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
 
   return (
     <div>
-      <h2>Form Component</h2>
-      <Form />
-      <hr />
-      <Like onClick={() => console.log("clicked")} />
-      <br />
-      <BsFillCalendarFill color="red" size="20" />
-      <hr />
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>
-          Hello <b>World</b>
-        </Alert>
-      )}
-      <Button
-        color="primary"
-        onClick={() => {
-          console.log("click");
-          setAlertVisibility(true);
-        }}
-      >
-        Click
-      </Button>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
+      <h2>Expense App</h2>
+      <div className="mb-5">
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
+      </div>
+
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       />
+      <div>
+        <select
+          className="form-select"
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value=""></option>
+          <option value="Clothing">Clothing</option>
+          <option value="Household">Household</option>
+        </select>
+        <ProductList category={category} />
+      </div>
       <hr />
+      <p>Users</p>
+      {error && <p className="text-danger">{error}</p>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
